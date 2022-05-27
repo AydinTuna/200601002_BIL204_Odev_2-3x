4eef7a8b6ef3ce9974430d4c55533aa23df6c44f
aydintuna <aydinbahadirtuna@gmail.com>
2022-05-27 23:15:24 +0300

    Add project files.

---
+++ 200601002_BIL204_Odev_2-3/Hash.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _200601002_BIL204_Odev_2_3
+{
+    public class Hash
+    {
+        public HashEleman[] HashDizi;
+
+        public Hash(Tree _tree)
+        {
+            HashDizi = new HashEleman[_tree.HeapDizi.Length];
+
+            foreach (TreeNode i in _tree.HeapDizi)
+            {
+                Insert(i);
+            }
+        }
+
+        public void Insert(TreeNode _tree)
+        {
+            int mod = _tree.SozcukKullanimSayisi % HashDizi.Length;
+
+            if (HashDizi[mod] == null)
+            {
+                HashDizi[mod] = new HashEleman(_tree.Sozcuk, _tree.SozcukKullanimSayisi / HashDizi.Length);
+            }
+            else
+            {
+                while (HashDizi[mod] != null)
+                {
+                    mod++;
+                }
+                HashDizi[mod] = new HashEleman(_tree.Sozcuk, _tree.SozcukKullanimSayisi / HashDizi.Length);
+            }
+        }
+    }
+}
